cfe76d67bf5646987ab606459467df5a0eb692db
Kaisa Huusko <kaisa.huusko@stuntfreaksteam.org>
2019-06-04 12:14:07 +0300

    CSS Module addition

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,14 +1,17 @@
 import React from "react"
+import { Link } from "gatsby"
+import Container from "../components/container"
 import Header from "../components/header"
 import Footer from "../components/footer"
 import PageHeader from "../components/pageHeader"
 
 export default () => (
-    <div id="site">
+    <Container >
         <Header />
         <body>
             <PageHeader headerText="About" />
+            <Link to="/">Index</Link>
         </body>
         <Footer />
-    </div>
+    </Container>
 )

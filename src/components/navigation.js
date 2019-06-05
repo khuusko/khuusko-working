3732988a2826396eef473102bc212f9be04a46b7
Kaisa Huusko <kaisa.huusko@stuntfreaksteam.org>
2019-06-05 11:38:37 +0300

    Navigation component

---
+++ src/components/navigation.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { Link } from "gatsby"
+
+export default() => (
+    <div>
+        <Link to="/">Index</Link>
+        <Link to="/about/">About</Link>
+        <Link to="/blog/">Blog</Link>
+    </div>
+)

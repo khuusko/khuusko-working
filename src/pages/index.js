8ce6a5e2faebd344192d1573d8875cd213d96165
Kaisa Huusko <kaisa.huusko@stuntfreaksteam.org>
2019-06-06 15:33:46 +0300

    Index and global adjusting

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,23 @@ import layoutStyles from "../components/layout.module.css"
 export default () => (
     <Container>
         <div className={layoutStyles.pageContent}>
-            <div className={layoutStyles.pHead}>
-                Index
-            </div>
             <div className={layoutStyles.bLeft}>
-                Kokoo
+                <h2>Index</h2>
+                <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
             </div>
             <div className={layoutStyles.bRight}>
-                Kokoo 2
+                <h2>Index</h2>
+                <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
             </div>
         </div>
     </Container>

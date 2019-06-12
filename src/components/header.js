5d7276a296a7454b9c9b77730870981b228ff570
Kaisa Huusko <kaisa.huusko@stuntfreaksteam.org>
2019-06-12 16:02:56 +0300

    Layout and colors

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,7 @@
 import React from "react"
 import {Link} from "gatsby"
 import headerStyles from "../components/header.module.css"
+import logo from "../images/glasses.png"
 
 export default () => (
     <header className={headerStyles.siteHeader}>
@@ -8,7 +9,7 @@ export default () => (
             <Link to="/about/">About</Link>
         </div>
         <div className={headerStyles.headerItem1}>
-            <h1>KAISA HUUSKO</h1>
+            <img src={logo} className={headerStyles.logoImg} alt="glasses-logo" />
         </div>
         <div className={headerStyles.headerItem3}>
             <Link to="/blog/">Blog</Link>
